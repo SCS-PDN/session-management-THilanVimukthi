c3f7f4ce31be3372f6490b2af21f75d1dcbffe7f
THilanVimukthi <s20347@sci.pdn.ac.lk>
2025-05-02 15:52:43 +0530

    Update LoginServlet.java

--- LoginServlet.java
+++ LoginServlet.java
@@ -1,19 +1,43 @@
-import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
+import java.io.IOException;
+import java.util.HashMap;
 
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+    private static final HashMap<String, String> users = new HashMap<>();
+
+    public void init() {
+        users.put("Thilan", "pass1");
+        users.put("Vimukthi", "pass2");
+        users.put("admin", "admin123");
+    }
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // TODO: Implement login logic
-        // 1. Get username & password from request
-        // 2. Validate credentials (hardcode a few users)
-        // 3. If valid:
-        //    - Create session
-        //    - Store username in cookie
-        //    - Redirect to DashboardServlet
-        // 4. If invalid, redirect back to login.html
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if (users.containsKey(username) && users.get(username).equals(password)) {
+
+            HttpSession session = request.getSession();
+            session.setAttribute("username", username);
+
+            Cookie userCookie = new Cookie("username", username);
+            userCookie.setMaxAge(60 * 60);
+            response.addCookie(userCookie);
+
+
+            response.sendRedirect("DashboardServlet");
+        } else {
+            response.setContentType("text/html");
+            response.getWriter().println("<h3>Invalid username or password!</h3>");
+            response.getWriter().println("<a href='login.html'>Try Again</a>");
+        }
+
+
     }
 }
